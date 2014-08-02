5e09ab28b236a4727452627ca15737320b246127
owen <owen@owenduffy.net>
2014-08-03 07:29:21 +1000

    Changes for v4.1, enhancements.

--- logwatch.pl
+++ logwatch.pl
@@ -6,15 +6,14 @@
 #
 # Chris Howard  w0ep   July 2005
 #
+# Fixes for javAPRSSrvr 4.1
+# Owen Duffy  VK2OMD  Aug 2014
+#
 
 use IO::Socket;
 use Getopt::Std;
 
-@DAYS = ('Sun', 'Mon', 'Tue', 'Wed','Thu','Fri','Sat');
-@MONTHS = ('JAN', 'FEB', 'MAR', 'APR','MAY','JUN','JUL',
-	'AUG','SEP','OCT','NOV','DEC');
-
-getopts('h:p:',\%opts);
+getopts('ah:l:p:',\%opts);
 
 if( defined($opts{'h'}) )
 {
@@ -28,7 +27,25 @@ if( defined($opts{'p'}) )
 }
 else { usage(); }
 
+
+if( defined($opts{'l'}) )
+{
+	$outfile = $opts{'l'};
+  if( defined($opts{'a'}) ){
+    open $OUT5,">>",$outfile or die "cannot open > $outfile: $!";
+  }
+  else{
+  open $OUT5,">",$outfile or die "cannot open > $outfile: $!";
+  }
+  autoflush $OUT5 1;
+}
+
+#so that it works properly in pipelines
+#$|=1;
+autoflush STDOUT 1;
+
 printf "making connection to host $host  port $port\n";
+printf(OUT "making connection to host $host  port $port\n");
 
 my $sock = new IO::Socket::INET (
                                   PeerAddr => $host,
@@ -41,8 +58,7 @@ printf "connected to host $host port $port\n";
 
 while ( $line = $sock->getline() )
 {
-	$line  =~ m/\[([<>][RI]:)(\d+)\](.*)/;
-
+	$line  =~ m/([<>])\t(\d+)\t(.*)/;
 	$one = $1;
 	$two = $2;
 	$three = $3;
@@ -50,19 +66,20 @@ while ( $line = $sock->getline() )
 	$milisec = $two - ($time * 1000);
 
 	($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst)
-		= localtime($time);
+		= gmtime($time);
 
-	$stamp = sprintf "%2.2d:%2.2d:%2.2d.%3.3d %s %02.2d-%3.3s-%04.4d",
-		$hour,$min,$sec, $milisec,
-		$DAYS[$wday],$mday, $MONTHS[$mon], $year+1900;
-	printf '['.$one.$stamp.']'.$three."\n";
+	$stamp = sprintf "%04.4d%02.2d%02.2d %2.2d:%2.2d:%2.2d.%3.3d",
+		$year+1900,$mon,$mday,$hour,$min,$sec,$milisec;
+	printf $one.$stamp."\t".$three."\n";
+  if($OUT5){
+	  printf($OUT5 $one.$stamp."\t".$three."\n");
+    }
 }
 close($sock);
-
 exit(0);
     
 sub usage
 {
-	printf STDERR "usage: $0 -h host -p port\n";
+	printf STDERR "usage: $0 -h host -p port [-l outputfile -a]\n";
 	exit -1;
 }
